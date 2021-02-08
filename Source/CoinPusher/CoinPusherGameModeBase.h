12d0e66dce37ddcf7c73b178adb56697c5b44cbf
Neekhaulas <nicolasroy96@gmail.com>
2021-02-08 10:35:43 +0100

    Initial commit

---
+++ Source/CoinPusher/CoinPusherGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoinPusherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COINPUSHER_API ACoinPusherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
